cb2f371c4ede46001e767b22133200a6a50f359b
Bin1512 <88291610+Bin1512@users.noreply.github.com>
2022-03-01 20:14:04 +0100

    Update costFunction.m

--- costFunction.m
+++ costFunction.m
@@ -21,7 +21,21 @@ grad = zeros(size(theta));
 %
 
 
-
+m = length(y); 
+z=X*theta;
+h=sigmoid(z);
+H=h.';
+h1=log(H)*(-y);
+h2=(log(1-H))*(1-y);
+J=(h1-h2)/m;
+s=h-y;
+s=s.';
+for i=1:3,
+    
+    grad(i)=(s)*X(:,i);
+    %grad(i)=temp(1);
+end
+grad=grad/m;
 
 
 
