beb7e7a93229d4f8ea8446feece26bf18786bb90
Bin1512 <88291610+Bin1512@users.noreply.github.com>
2022-03-01 20:14:47 +0100

    Update costFunctionReg.m

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -18,6 +18,12 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+hx = sigmoid(X * theta);
+m = length(X);
+
+J = (sum(-y' * log(hx) - (1 - y')*log(1 - hx)) / m) + lambda * sum(theta(2:end).^2) / (2*m);
+grad =((hx - y)' * X / m)' + lambda .* theta .* [0; ones(length(theta)-1, 1)] ./ m ;
+
 
 
 
